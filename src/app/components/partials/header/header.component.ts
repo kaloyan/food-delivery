3959aded6a0fadfa2ca3c487af623484b016ef59
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-11-20 17:52:45 +0200

    Fixed cart item in nemebar to show correct cart items count

--- src/app/components/partials/header/header.component.ts
+++ src/app/components/partials/header/header.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  cartItemsCount = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(cartService: CartService) {
+    cartService.getCartObservable().subscribe((cart) => {
+      this.cartItemsCount = cart.totalCount;
+    });
   }
 
+  ngOnInit(): void {}
 }
