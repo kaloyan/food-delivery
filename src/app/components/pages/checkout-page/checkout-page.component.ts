078211c8fc6622dc9d1c855652129f958ff4716d
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-12-08 21:05:09 +0200

    Checkout page refactoring and UI redesign

--- src/app/components/pages/checkout-page/checkout-page.component.ts
+++ src/app/components/pages/checkout-page/checkout-page.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CartService } from 'src/app/services/cart.service';
 import { OrderService } from 'src/app/services/order.service';
 import { ToastService } from 'src/app/services/toast.service';
 import { UserService } from 'src/app/services/user.service';
 import { Order } from 'src/app/shared/models/Order';
+import { User } from 'src/app/shared/models/User';
 
 @Component({
   selector: 'app-checkout-page',
@@ -14,12 +14,11 @@ import { Order } from 'src/app/shared/models/Order';
 })
 export class CheckoutPageComponent implements OnInit {
   order: Order = new Order();
-  checkoutForm!: FormGroup;
+  user!: User;
 
   constructor(
     cartService: CartService,
     private router: Router,
-    private formBuilder: FormBuilder,
     private userService: UserService,
     private toastService: ToastService,
     private orderService: OrderService
@@ -30,24 +29,10 @@ export class CheckoutPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    let { name, address } = this.userService.currentUser;
-
-    this.checkoutForm = this.formBuilder.group({
-      name: [name, Validators.required],
-      address: [address, Validators.required],
-    });
-  }
-
-  get formControls() {
-    return this.checkoutForm.controls;
+    this.user = this.userService.currentUser;
   }
 
   createOrder() {
-    if (this.checkoutForm.invalid) {
-      this.toastService.showWarningToast('Warning', 'Please fill the inputs');
-      return;
-    }
-
     if (!this.order.latlong) {
       this.toastService.showWarningToast(
         'Waning: No location',
@@ -56,8 +41,8 @@ export class CheckoutPageComponent implements OnInit {
       return;
     }
 
-    this.order.name = this.formControls.name.value;
-    this.order.address = this.formControls.address.value;
+    this.order.name = this.user.name;
+    this.order.address = this.user.address;
 
     // post new order on server
     this.orderService.create(this.order).subscribe({
